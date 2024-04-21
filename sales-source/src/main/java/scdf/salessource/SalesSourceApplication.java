d2674c0dfb9b1f3a1f1387d44d128b9b51d41a61
Daniel Lopes <>
2024-04-21 15:10:58 +0100

    repo init

---
+++ sales-source/src/main/java/scdf/salessource/SalesSourceApplication.java
@@ -0,0 +1,13 @@
+package scdf.salessource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSourceApplication.class, args);
+	}
+
+}
---
+++ sales-source/src/test/java/scdf/salessource/SalesSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package scdf.salessource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
